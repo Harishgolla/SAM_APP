0d03fa79bc9c7062976eb531dbeed278585fed5c
HARISH KUMAR GOLLA <harish.golla@nutanix.com>
2018-03-27 10:53:19 +0530

    Modified the code

---
+++ api/controllers/TestController.js
@@ -0,0 +1,58 @@
+module.exports = {  
+  /**
+   * Validate the authenticated state of the requester.
+   *
+   * @param {Object} req
+   * @param {Object} res
+   */
+  postData: function (req, res) {
+    var data = {
+      method: 'post',
+      query: req.query,
+      body: req.body,
+      headers: req.headers,
+      cookie: req.cookies,
+      route: req.route
+    };
+    //res.status(400);
+    //res.view('400', {message: 'Sorry, you need to tell us the ID of the FOO you want!'});
+    console.log(req.body);
+    res.ok(data);
+  },
+  putData: function (req, res) {
+    var data = {
+      method: 'put',
+      query: req.query,
+      body: req.body,
+      headers: req.headers,
+      cookie: req.cookies,
+      route: req.route
+    };
+    console.log(req.body);
+    res.ok(data);
+  },
+  getData: function (req, res) {
+    var data = {
+      method: 'get',
+      query: req.query,
+      body: req.body,
+      headers: req.headers,
+      cookie: req.cookies,
+      route: req.route
+    };
+
+    res.ok(data);
+  },
+  deleteData: function (req, res) {
+    var data = {
+      method: 'delete',
+      query: req.query,
+      body: req.body,
+      headers: req.headers,
+      cookie: req.cookies,
+      route: req.route
+    };
+
+    res.ok(data);
+  }
+}
